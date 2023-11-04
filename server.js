cc8cb63fead79b2af154ecb037f80ea8dda6cc40
warren tapawan <wtapawan@gmail.com>
2023-11-04 15:50:51 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express()
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/person', (req, res) => {
+    const person = {
+        "name": "John Doe",
+        "age": 30,
+        "email": "john.doe@example.com"
+    }
+    res.json(person);
+});
+
+// Start Node
+app.listen(PORT, ()=>{
+    console.log(`it's alive on https://localhost:${PORT}`)
+})
